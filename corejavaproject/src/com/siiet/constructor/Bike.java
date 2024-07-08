3a0caa046af64989af36aca0f5cafa87f86fe5be
Vamshi <Vamshi@192.168.0.139>
2024-07-08 15:55:08 +0530

    commit

---
+++ corejavaproject/src/com/siiet/constructor/Bike.java
@@ -0,0 +1,16 @@
+package com.siiet.constructor;
+
+public class Bike {
+	Bike()
+
+	{
+		System.out.println("default constructor");
+	}
+		public static void main(String[] args) {
+		
+			Bike b=new Bike();
+			
+		}
+		
+	}
+
